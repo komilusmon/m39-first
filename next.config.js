eb6915558c07b40c381c31f541d79723a46a6e58
Komil <usmonqulovkomil80@gmail.com>
2026-08-02 18:12:08 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@
 const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
+  transpilePackages: ['firebase'],
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
-        ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
         dns: false,
         child_process: false,
+        undici: false,
       };
     }
     return config;
