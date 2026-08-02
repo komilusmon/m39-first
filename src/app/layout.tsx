08db4585ea871466cb75fe51b17a2cdbe30fa585
Komil <usmonqulovkomil80@gmail.com>
2026-08-02 18:24:17 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'M39 Yo\'l Yordam - Tezkor Yordam Xizmati',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="uz">
-      <body className={inter.className}>
+      <body>
         {children}
         <Toaster position="top-center" />
       </body>
